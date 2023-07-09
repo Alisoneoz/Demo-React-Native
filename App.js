19e1b7d6e47405ccf0f77810a4141bdadfd08583
alisoneoz <alisonestephanyoz@gmail.com>
2023-07-08 23:05:32 -0400

    Adding Basic Components and Style

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor='gray'/>
+      <Image 
+        source={{uri:'https://reactnative.dev/docs/assets/GettingStartedCongratulations.png'}}
+        style={styles.image}
+      />
+      <Text style={{color:"green", fontWeight:"bold", fontSize:30, textAlign:"center"}}>Hola mandy mira este es Dukie el gatito de Michi</Text>
+      
+      <Image 
+        source={{uri:'https://reactnative.dev/docs/assets/p_cat2.png'}}
+        style={styles.image}
+      />
+      <Text style={styles.text}>Por cierto... Michi tiene dientes de Coneeejoo</Text>
+      <TextInput style={styles.input}/>
     </View>
   );
 }
@@ -13,8 +24,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color:'purple',
+    fontSize:15,
+    fontWeight:"bold",
+    padding:20
+  },
+  input:{
+    backgroundColor:"aqua",
+    borderRadius:15,
+    paddingVertical: 10,
+    paddingHorizontal:100,
+  },
+  image:{
+    height:250,
+    width:250,
+  }
 });
